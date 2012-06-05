7c0231add183cc709efb1e37d8e28025b2cfd29c
Marshal Horn <kamocat@gmail.com>
2012-06-05 01:00:55 -0700

    got PWM working

--- ork/main.c
+++ ork/main.c
@@ -5,137 +5,35 @@
 
 /** Constants */
 #define F_CPU 1000000U
-// #define MOTOR_DIVIDE 3	// only use 1/3 full voltage
-// #define INTERRUPT_DRIVEN_UART	// use interrupts for UART receive
-#define MSG_LENGTH 2	// there are only 3 bytes in our message
-// #define WUNDERBOARD // we are testing on the tekpet right now
-// #define WD_TIMEOUT 1000 // watchdog timeout after about 1 second
 
 /** Includes */
-#include "watchdog.h"
 #include <avr/io.h>
 #include <util/delay.h>
-#include "servo_pwm.h"	// enables sevo-style PWM control
-// #include "dc_motor.h"	// enables control of DC motors
-#include "uart.h"	// for the serial communication
+#include "vic.h"	// enables sevo-style PWM control
 
 /** Global Variables */
-int8_t lspeed;
-int8_t rspeed;
-int8_t servo_angle;
 
 /** Functions */
 
 uint8_t init( void ) {
-	init_watchdog();
-	//init_UART();
+	init_vic();
 
-#ifdef WUNDERBOARD
-	DDRB = 0b11000000;
-	DDRC = 0xFF;	// LEDs are all outputs
 
-	/* Clear display */
-	PORTB = 0b11000000;
-	PORTC = 0;
-
-	PORTB = 0b01000000;	//only display red
-	PORTC = 0x80;	// display SOMETHING so we know it works
-
-	/* Let programmer know what happened */
-	send_string("Device was reset\r\n");
-#else
-	init_servos();
-	//init_dcmotors( 1 );
-#endif
-
-	// start interrupts
-	sei();
 
 	return 0;
 }
 
 
-#ifdef INTERRUPT_DRIVEN_UART
-/***************************************************
- * For interrupt handling, run sei() and paste this code into your 
- * main.c file.  The surrounding "ifdef" is neccessary because it
- * is used in the header to determine whether to enable the UART
- * RCV interrupts.  (Transmit is currently not interrupt-driven).
- */
-
-ISR( BADISR_vect ) {
-	// Do nothing
-}
-
-ISR( USART1_RX_vect ) {
-	byte_received = UDR1;	// copy the data before it goes away
-	// Use 255 to signal a new message
-	if( byte_received == 254 ) {
-		// Reset the counter and clear the array
-		for( byte_index = 0; byte_index < MSG_LENGTH; ++byte_index ) {
-			uart_rcvd[ (int) byte_index ] = 0;
-		}
-		byte_index = 0;
-	} else if( byte_index < MSG_LENGTH ){
-		// update the data
-		uart_rcvd[ (int) byte_index ] = byte_received;
-		byte_index = ( byte_index + 1 );
-
-		// If this is the last byte, update the buffered data.
-		if( byte_index == MSG_LENGTH ) {
-			lspeed = uart_rcvd[0] - 127;	// centers on 127
-			rspeed = uart_rcvd[1] - 127;	// centers on 127
-			inc_servo( (uart_rcvd[2] - 2),	// centers on 2
-					&servo_angle );	
-			
-			// Reset the watchdog
-			wdt_reset();
-		}
-	}
-
-
-}
-/*********** End interrupt-driven UART ***********/
-#endif
-
-
-ISR( WDT_vect ) {
-	/* 
-	 * The watchdog has timed out, which means wdt_reset() hasn't 
-	 * been called recently, so something isn't happening as often as we 
-	 * think it should (depending on where wds_reset() is called).
-	 * Do something to respond to that:
-	 */
-#ifdef WUNDERBOARD
-	send_string("WDT ");
-#else
-	/*
-	 * We lost communication.
-	 * Since this blimp is naturally heavy, we can just shut off the
-	 * motors, and it should come down.  However, if it turns out the
-	 * craft doesn't sink fast enough, we can change this to actually
-	 * drive the motors down.  These values won't be overwritten by
-	 * anything else until a new message is received.
-	 */
-	servo_angle = 0;
-	lspeed = 0;
-	rspeed = 0;
-#endif
-
-
-}
-
-
 int main( void ) {
-	init();
+	int16_t lspeed = 1000;
+	int16_t rspeed = -1000;
 
-	uint8_t i = 0;
 	while( 1 ) {
-		/* Motor control not merged yet */
-		set_servo( i, &OCR1A );
-		++i;
-		//run_dcmotors( lspeed, rspeed );
-		_delay_ms( 20 );	// wait long enough for the servo to update
+		set_motor( &OCR1A, lspeed );
+		set_motor( &OCR1B, rspeed );
+		set_motor( &OCR1C, lspeed );
+		set_motor( &OCR3A, rspeed );
+		_delay_ms(100);
 	}
 	return 0;
 }
---
+++ ork/vic.h
@@ -0,0 +1,38 @@
+#ifndef ORK_VIC
+#define ORK_VIC
+
+#include <avr/io.h>
+uint8_t init_vic( void ) {
+	DDRB |= 0xE0;	// turn on PWM outputs
+	TCCR1A = 0b10101010;	// fast PWM, 16 bit
+	TCCR3A = 0b10000010;
+
+	TCCR1B = 0b00011010;	// prescaler of 1/8
+	TCCR3B = 0b00011010;
+
+	ICR1 = 40000;	// update every 20 ms
+	ICR3 = 40000;
+
+	// Make sure the registers aren't disabled for sleep mode
+	PRR0 &= ~(1<<PRTIM1);
+	PRR0 &= ~(1<<PRTIM3);
+
+
+	return 0;
+}
+
+
+/*
+ * This set_motor function takes a range from -1000 to 1000, where
+ * -1000 is full-reverse, 1000 is full-forward, and 0 is stopped.
+ *  If you give it a value out of this range, it won't complain or
+ *  catch the error.
+ */
+uint8_t set_vic( volatile uint16_t *motor, int16_t speed ) {
+	// We want a range from 1 to 2 ms.  1.5ms is 3000
+	*motor = speed + 3000;
+	return 0;
+}
+
+
+#endif
